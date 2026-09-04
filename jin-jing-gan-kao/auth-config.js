cb4a00aeaac6b2909a3660fe9c7f5aec330ac07d
Potatobox062416 <2872828299@qq.com>
2026-09-04 14:44:20 +0800

    feat: enable Supabase account authentication

--- jin-jing-gan-kao/auth-config.js
+++ jin-jing-gan-kao/auth-config.js
@@ -1,6 +1,6 @@
 window.AUTH_CONFIG = Object.freeze({
-  supabaseUrl: "",
-  supabasePublishableKey: "",
+  supabaseUrl: "https://fidtelkdzygunsmpnmgl.supabase.co",
+  supabasePublishableKey: "sb_publishable_7k_GlEWR-vISYHawjYTuNQ_aZq4NvNH",
   siteUrl: "https://potatobox062416.github.io/AI/jin-jing-gan-kao/"
 });
 
